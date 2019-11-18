ed5d09c11a84750a2f987c6887d88513021ccc27
reacotay <anton.hallqvist1@gmail.com>
2019-11-18 13:28:04 +0100

    Waterfall done

---
+++ Assets/Waterfall/Scripts/Scrollspeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrollspeed : MonoBehaviour
+{
+    public float ScrollX;
+    public float ScrollY;
+
+    private float offsetX;
+    private float offsetY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        offsetX = Time.time * ScrollX;
+        offsetY = Time.time * ScrollY;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
